7113ff464766adc7bd9a260d3427dda84be74c28
Otavio-Parraga <otavio.parraga@acad.pucrs.br>
2021-01-16 03:38:15 -0300

    Updates

---
+++ precache-manifest.db9e27af44bed9b88c6f45e4a699438a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbad63053de7ad151b192a58a5940ba",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b64c67185f625f02251d",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64c67185f625f02251d",
+    "url": "/home/static/js/main.c3fade23.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "917437ffb3bf0534147248a07534a05f",
+    "url": "/home/static/media/eu.917437ff.jpg"
+  },
+  {
+    "revision": "9768761249b76f82c0f39ff682d57bb8",
+    "url": "/home/static/media/resume.97687612.pdf"
+  }
+]);
